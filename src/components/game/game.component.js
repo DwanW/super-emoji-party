efb7a424df82ae76dee85c551f4e3f701955e8c0
DwanW <kunvang@yahoo.com>
2020-03-30 16:47:35 -0600

    dynamically added delay

--- src/components/game/game.component.js
+++ src/components/game/game.component.js
@@ -3,13 +3,20 @@ import './game.styles.scss';
 
 //this class sets up board and dom event listeners.
 class GameBoard extends React.Component {
-  onClick() {
-    console.log(this);
-    this.props.moves.rollDie();
+  async onClick() {
+    // this.props.moves.traverse()
+    await this.props.moves.rollDie();
+    // let currentPlayer = this.props.G.players[Number(this.props.ctx.currentPlayer)];
+    let rollValue = this.props.G.dieRoll;
+    for (let i = 0; i < rollValue; i++) {
+      setTimeout(() => this.props.moves.traverse(), i * 1000);
+    }
     this.props.events.endTurn();
   }
 
   render() {
+    console.log(this.props);
+    // generate cell;
     const cellWidth = 50;
     const cellHeight = 50;
 
@@ -23,11 +30,13 @@ class GameBoard extends React.Component {
       position: "absolute",
     };
 
+    // generate board;
     const board = [[0, 1], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5]];
     // translate into an array of node
     // Generates board with Top Left at 0,0
     const boardNode = board.map((e, idx) => (<div style={{ ...cellStyle, top: `${cellHeight * e[1]}px`, left: `${cellWidth * e[0]}px` }} key={idx} >{this.props.G.spaces[idx]}</div>))
 
+    // f
     let winner = null;
     if (this.props.ctx.gameover) {
       winner =
@@ -43,7 +52,7 @@ class GameBoard extends React.Component {
         <div style={{ position: 'relative' }}>
           {boardNode}
         </div>
-        {/* arrow function grabs this */}
+
         <button onClick={() => this.onClick()}>roll dice</button>
         <div>{winner}</div>
       </React.Fragment>
@@ -58,26 +67,35 @@ const emojiParty = {
     // change 10 later
     spaces: Array(10).fill(null),
     dieRoll: 1,
-    player1Obj: { position: 0, equipment: null, ailment: null, buff: null, inventory: [] },
-    player2Obj: { position: 0, equipment: null, ailment: null, buff: null, inventory: [] },
+    players: [{ position: 0 }, { position: 0 }]
   }),
+  // phase: {
+  //   rollDie:{
+
+  //   }
+  // },
   moves: {
     rollDie: (G, ctx) => {
-      G.spaces[G.player1Obj.position] = null;
-      G.spaces[G.player2Obj.position] = null;
       G.dieRoll = ctx.random.Die(6);
-      ctx.currentPlayer === "0" ? G.player1Obj.position += G.dieRoll : G.player2Obj.position += G.dieRoll;
-      if (G.player1Obj.position !== G.player2Obj.position) {
-        G.spaces[G.player1Obj.position] = "p1";
-        G.spaces[G.player2Obj.position] = "p2";
-      } else if (G.player1Obj.position === G.player2Obj.position) {
-        G.spaces[G.player1Obj.position] = "p1p2";
-      }
+      // ctx.currentPlayer === "0" ? G.player1Obj.position += G.dieRoll : G.player2Obj.position += G.dieRoll;
+      // if (G.player1Obj.position !== G.player2Obj.position) {
+      //   G.spaces[G.player1Obj.position] = "p1";
+      //   G.spaces[G.player2Obj.position] = "p2";
+      // } else if (G.player1Obj.position === G.player2Obj.position) {
+      //   G.spaces[G.player1Obj.position] = "p1p2";
+      // }
     },
+    traverse: (G, ctx) => {
+      let currentPlayer = G.players[Number(ctx.currentPlayer)];
+      G.spaces[currentPlayer.position] = null;
+      currentPlayer.position++;
+      G.spaces[currentPlayer.position] = "player";
+    }
   },
   endIf: (G, ctx) => {
     // change 10 later
-    if (G.player1Obj.position > 10 || G.player2Obj.position > 10) {
+    let currentPlayer = G.players[Number(ctx.currentPlayer)];
+    if (currentPlayer.position > 9) {
       return { winner: ctx.currentPlayer };
     }
   },
